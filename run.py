addc2ee385cd30e4db24d48fec316f326e6ea4a8
Rob Gannon <cyberpeaches@gmail.com>
2022-04-21 11:57:16 +0000

    create class for boards

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class create_game_board:
+    def __init__(self, size, ships):
+        self.size = size
+        self.ships = ships
+
+    def build_board(self):
+        """
+        builds the board to be printed
+        """
+
+        board_matrix = []
+        for row in range( 0, self.size ):
+            row_list = []
+            for col in range( 0, self.size ):
+                row_list.append('.')
+            board_matrix.append(row_list)
+            row_list = []
+        return board_matrix
+
+
+def run_game():
+    my_board = create_game_board( 5, 4 )
+    print(my_board.build_board())
+
+
+run_game()
