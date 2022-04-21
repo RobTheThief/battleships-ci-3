f8ee28271c0ad1e7127a5bbe93c0511fc880d7d5
Rob Gannon <cyberpeaches@gmail.com>
2022-04-21 14:39:17 +0000

    create recieve_shot method

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ class create_game_board:
         self.size = size
         self.ships = ships
         self.board_matrix = []
+        self.hit_count = 0
         
 
     def build_board(self):
@@ -31,9 +32,16 @@ class create_game_board:
             while not done:
                 random_row = get_random(self.size)
                 random_col = get_random(self.size)
-                if self.board_matrix[random_row][random_col] != '{]':
-                    self.board_matrix[random_row][random_col] = '{]'
+                if self.board_matrix[random_row][random_col] != '<>':
+                    self.board_matrix[random_row][random_col] = '<>'
                     done = True
+
+    def take_shot(self, row, col):
+        if self.board_matrix[row - 1][col - 1] == '<>':
+            self.board_matrix[row - 1][col - 1] = ' #'
+            self.hit_count += 1
+        else:
+            self.board_matrix[row - 1][col - 1] = ' X'
 
 def get_random(size):
     '''
@@ -58,6 +66,9 @@ def run_game():
     my_board = create_game_board( 6, 7 )
     my_board.add_ships()
     print_board(my_board.board_matrix)
+    my_board.take_shot(3,3)
+    print_board(my_board.board_matrix)
+    print(my_board.hit_count)
     print('hi')
 
 
