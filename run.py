95d193f95862a201573bfd70d0e4d6fbab4c9db7
Rob Gannon <cyberpeaches@gmail.com>
2022-04-21 13:55:42 +0000

    create add_ships method

--- run.py
+++ run.py
@@ -5,21 +5,35 @@ class create_game_board:
     def __init__(self, size, ships):
         self.size = size
         self.ships = ships
+        self.board_matrix = []
+        
 
     def build_board(self):
         """
-        builds the board to be printed
+        builds the board matrix according to size
         """
 
-        board_matrix = []
         for row in range( 0, self.size ):
             row_list = []
             for col in range( 0, self.size ):
-                row_list.append('.')
-            board_matrix.append(row_list)
+                row_list.append(' .')
+            self.board_matrix.append(row_list)
             row_list = []
-        return board_matrix
 
+    def add_ships(self):
+        """
+        adds ships to unique random coordinates according to ship number
+        """
+
+        self.build_board()
+        for _ in range(0, self.ships):
+            done = False
+            while not done:
+                random_row = get_random(self.size)
+                random_col = get_random(self.size)
+                if self.board_matrix[random_row][random_col] != '{]':
+                    self.board_matrix[random_row][random_col] = '{]'
+                    done = True
 
 def get_random(size):
     '''
@@ -28,19 +42,23 @@ def get_random(size):
     '''
     return random.randint(1, size)
 
-
 def print_board(board):
+    '''
+        Formats the given board matrix row into a series of strings and 
+        prints out each row in order
+    '''
     rows = []
     for row in board:
-        x = "   ".join(row)
+        x = "  ".join(row)
         rows.append(x)
     for row in rows:
         print(row, '\n')
 
 def run_game():
-    my_board = create_game_board( 6, 4 )
-    print_board(my_board.build_board())
-    print(get_random(my_board.size))
+    my_board = create_game_board( 6, 7 )
+    my_board.add_ships()
+    print_board(my_board.board_matrix)
+    print('hi')
 
 
 run_game()
