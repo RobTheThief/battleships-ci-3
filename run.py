80f2259ea08f08d311d1629c6f853c7c91bb9624
Rob Gannon <cyberpeaches@gmail.com>
2022-05-01 20:24:36 +0000

    correct spaces from pep8 errors

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import copy
 from helpers import *
 from api_calls import *
 
+
 class create_game_board:
-    def __init__(self, size, ships, name = 'Computer'):
+    def __init__(self, size, ships, name='Computer'):
         self.size = size
         self.ships = ships
         self.name = name
@@ -23,8 +24,8 @@ class create_game_board:
         to the board object twice. The second is deep copied
         and used as a blank board for the opponent.
         '''
-        for row in range( 0, self.size ):
-            row_list = [' .' for cell in range( 0, self.size )]
+        for row in range(0, self.size):
+            row_list = [' .' for cell in range(0, self.size)]
             self.board_matrix.append(row_list)
         self.board_matrix_obscured = copy.deepcopy(self.board_matrix)
 
@@ -42,7 +43,7 @@ class create_game_board:
                 if self.board_matrix[random_row][random_col] != '<>':
                     self.board_matrix[random_row][random_col] = '<>'
                     done = True
-        if self.name != 'Computer':            
+        if self.name != 'Computer':
             self.login_player()
 
     def recieve_shot(self, row, col):
@@ -66,7 +67,7 @@ class create_game_board:
         """
             Finds which row the players data is on
             and saves the data and the row number to
-            the board object. 
+            the board object.
         """
         data = get_data()
         all_values = data[0]
@@ -85,7 +86,7 @@ class create_game_board:
         return found
 
     def add_new_player(self):
-        """ 
+        """
             Creates new player in the sheet and saves the
             current_history to the board object
         """
@@ -94,20 +95,20 @@ class create_game_board:
         scores = data[1]
         pword = input('New player! Enter a password:\n')
         length = len(all_values)
-       
-        update_cell(f'A{length + 1}', self.name, scores) #Name
-        update_cell(f'B{length + 1}', 0, scores) #Wins
-        update_cell(f'C{length + 1}', 0, scores) #Losses
-        update_cell(f'D{length + 1}', 0, scores) #Current Streak
-        update_cell(f'E{length + 1}', 0, scores) #Best Streak
-        update_cell(f'F{length + 1}', pword, scores) #Password
+
+        update_cell(f'A{length + 1}', self.name, scores)  # Name
+        update_cell(f'B{length + 1}', 0, scores)  # Wins
+        update_cell(f'C{length + 1}', 0, scores)  # Losses
+        update_cell(f'D{length + 1}', 0, scores)  # Current Streak
+        update_cell(f'E{length + 1}', 0, scores)  # Best Streak
+        update_cell(f'F{length + 1}', pword, scores)  # Password
 
         data = get_data()
         all_values = data[0]
         self.current_history = all_values[length - 1]
 
     def login_player(self):
-        """ 
+        """
             Checks if player has an account and logs them in.
             If no player found then redericted to create new player.
         """
@@ -119,7 +120,7 @@ class create_game_board:
             while password_wrong:
                 pword = input('Enter your password:\n')
                 if pword == self.current_history[5]:
-                    print('PREVIOUS SCORES: ','WINS', self.current_history[1], 'LOSSES', self.current_history[2])
+                    print('PREVIOUS SCORES: ', 'WINS', self.current_history[1], 'LOSSES', self.current_history[2])
                     print('CURRENT WIN STREAK', self.current_history[3], 'BEST STREAK', self.current_history[4])
                     password_wrong = False
                     break
@@ -127,7 +128,7 @@ class create_game_board:
                 run_game()
 
     def update_player_scores(self, win, loss):
-        """ 
+        """
             Updates the database for player for wins, losses, current streak,
             best streak.
         """
@@ -135,20 +136,21 @@ class create_game_board:
         scores = get_data()[1]
 
         win_update = int(self.current_history[1]) + int(win)
-        update_cell(f'B{self.data_row + 1}', win_update, scores) #Wins
+        update_cell(f'B{self.data_row + 1}', win_update, scores)  # Wins
 
         loss_update = int(self.current_history[2]) + int(loss)
-        update_cell(f'C{self.data_row + 1}', loss_update, scores) #Lossses
+        update_cell(f'C{self.data_row + 1}', loss_update, scores)  # Lossses
 
         if loss == 1:
-            update_cell(f'D{self.data_row + 1}', 0, scores) #Current Streak reset to 0
+            update_cell(f'D{self.data_row + 1}', 0, scores)  # Current Streak reset to 0
         elif win == 1:
             streak_update = int(self.current_history[3]) + int(1)
-            update_cell(f'D{self.data_row + 1}', streak_update, scores) #Current Streak increase
+            update_cell(f'D{self.data_row + 1}', streak_update, scores)  # C urrent Streak increase
         all_values = get_data()[0]
         if int(all_values[self.data_row][3]) > int(self.current_history[4]):
             best_streak = int(all_values[self.data_row][3])
-            update_cell(f'E{self.data_row + 1}', best_streak, scores) #Best Streak update
+            update_cell(f'E{self.data_row + 1}', best_streak, scores)  # Best Streak update
+
 
 def run_game():
     """
